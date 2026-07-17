d7513c6ce87614fe305d1cddcea9f0e707d1725c
s03qoj4vvn <s03qoj4vvn@quickmails.eu>
2026-07-17 11:12:00 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,17 +1,22 @@
 const WebSocket = require('ws');
 const net = require('net');
 
-const server = new WebSocket.Server({ port: 8080, path: '/ws' });
+const server = new WebSocket.Server({ port: 8080 });
 
 server.on('connection', (ws) => {
-  console.log('Browser connected to proxy');
+  console.log('[Proxy] Browser connected');
+
   const tcp = net.createConnection(4629, 'dagnam.xyz');
 
+  tcp.on('connect', () => console.log('[Proxy] Connected to pool dagnam.xyz:4629'));
   tcp.on('data', (data) => ws.send(data));
   ws.on('message', (data) => tcp.write(data));
 
   ws.on('close', () => tcp.destroy());
-  tcp.on('close', () => ws.close());
+  tcp.on('close', () => {
+    console.log('[Proxy] Pool disconnected');
+    ws.close();
+  });
 });
 
-console.log('Stratum Proxy running on ws://localhost:8080/ws');
+console.log('[Proxy] Listening on ws://localhost:8080');
