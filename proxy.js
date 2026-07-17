2424844820528efac55dabcd45a8cb7c64e053d5
s03qoj4vvn <s03qoj4vvn@quickmails.eu>
2026-07-17 13:46:06 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,25 +1,24 @@
 const WebSocket = require('ws');
+const https = require('https');
+const fs = require('fs');
 const net = require('net');
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ 
+  port: 8080,
+  path: '/ws'
+});
 
 server.on('connection', (ws) => {
-  console.log('Browser connected');
+  console.log('Browser connected via WSS');
 
   const tcp = net.createConnection(4629, 'dagnam.xyz');
 
-  tcp.on('connect', () => console.log('✅ Connected to pool'));
-
-  tcp.on('data', (data) => {
-    ws.send(data);
-  });
-
-  ws.on('message', (data) => {
-    tcp.write(data);
-  });
+  tcp.on('connect', () => console.log('Connected to pool'));
+  tcp.on('data', (data) => ws.send(data));
+  ws.on('message', (data) => tcp.write(data));
 
   ws.on('close', () => tcp.destroy());
   tcp.on('close', () => ws.close());
 });
 
-console.log('Proxy running on port 8080');
+console.log('WSS Proxy running on wss://localhost:8080/ws');
