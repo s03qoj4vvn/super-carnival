e5e92209cb532a2a03213aa0ed2278007569c8fb
s03qoj4vvn <s03qoj4vvn@quickmails.eu>
2026-07-17 11:15:54 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,22 +1,31 @@
 const WebSocket = require('ws');
 const net = require('net');
 
+console.log('Proxy starting...');
+
 const server = new WebSocket.Server({ port: 8080 });
 
 server.on('connection', (ws) => {
-  console.log('[Proxy] Browser connected');
+  console.log('Browser connected to proxy');
 
   const tcp = net.createConnection(4629, 'dagnam.xyz');
 
-  tcp.on('connect', () => console.log('[Proxy] Connected to pool dagnam.xyz:4629'));
-  tcp.on('data', (data) => ws.send(data));
-  ws.on('message', (data) => tcp.write(data));
+  tcp.on('connect', () => {
+    console.log('✅ Connected to pool dagnam.xyz:4629');
+  });
+
+  tcp.on('data', (data) => {
+    console.log('Pool -> Browser:', data.toString().slice(0,100));
+    ws.send(data);
+  });
+
+  ws.on('message', (data) => {
+    console.log('Browser -> Pool:', data.toString().slice(0,100));
+    tcp.write(data);
+  });
 
   ws.on('close', () => tcp.destroy());
-  tcp.on('close', () => {
-    console.log('[Proxy] Pool disconnected');
-    ws.close();
-  });
+  tcp.on('close', () => ws.close());
 });
 
-console.log('[Proxy] Listening on ws://localhost:8080');
+console.log('Proxy listening on port 8080');
